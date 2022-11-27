d678556e4ef8702a044b203170171b04757384f5
Federico Panarotto <104077886+federicopanarotto@users.noreply.github.com>
2022-11-27 19:47:45 +0100

    add capitolo3

---
+++ script.ps1
@@ -0,0 +1,15 @@
+$min=1;
+$max=14;
+$arg="argomento.html"
+$path="../../"
+$totalpath=''+$path+$arg
+
+for (; $min -lt $max+1; $min++) {
+    if ($min -lt 10) {
+        $filename="0"+$min+"_"+$arg
+    } else {
+        $filename=""+$min+"_"+$arg
+    }
+    cp $totalpath $filename
+}
+
