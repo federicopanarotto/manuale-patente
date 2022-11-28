c4051818219722f0f4a4a3c57395e70bc616c2b8
Federico Panarotto <104077886+federicopanarotto@users.noreply.github.com>
2022-11-28 23:30:25 +0100

    create all dirs

---
+++ automatic_script.ps1
@@ -0,0 +1,19 @@
+$home_path = 'C:\Users\federick\Documents\Dev\GitHub\patente\manuale-patente';
+
+Set-Location $home_path;
+
+.\script.ps1 1  8   11;
+.\script.ps1 1  4   12;
+.\script.ps1 1  17  13;
+.\script.ps1 1  41  14;
+.\script.ps1 1  5   15;
+.\script.ps1 1  4   16;
+.\script.ps1 1  13  17;
+.\script.ps1 1  16  18;
+.\script.ps1 1  4   19;
+.\script.ps1 1  16  20;
+.\script.ps1 1  6   21;
+.\script.ps1 1  8   22;
+.\script.ps1 1  2   23;
+.\script.ps1 1  4   24;
+.\script.ps1 1  11  25;
--- script.ps1
+++ script.ps1
@@ -1,32 +1,48 @@
 # argoments variables passed by command line
-$min=$args[0];
-$max=$args[1];
-$capNum=$args[2];
+$min_num = $args[0];
+$max_num = $args[1];
+$cap_num = $args[2];
 
 # Custom variables
-$copyFile='argomento.html';
-$path_to_file='../../';
-$totalpath=''+$path_to_file+$copyFile;
+$home_path = 'C:\Users\federick\Documents\Dev\GitHub\patente\manuale-patente';
+$argomento_file = 'argomento.html';
+$back_path = '../../';
+$totalpath = ''+$back_path+$argomento_file;
+$newDir = 'capitolo'+$cap_num;
 
-# Create img directory if not present
-mkdir img;
+# Create directories if non present
+Set-Location $home_path;
+Set-Location 'capitoli';
+if (Test-Path -Path $newDir) {
+    "Directory already exists!";
+} else {
+    mkdir $newDir >> cache.txt;
+}
+Set-Location $newDir;
+if (Test-Path -Path 'img') {
+    "Directory already exists!";
+} else {
+    mkdir img >> '../cache.txt';
+}
+
+Write-Host '';
 
 # Generate "Capitolo" file
-$capfileName='capitolo'+$capNum+'.html';
-$capfilePath=''+$path_to_file+"capitolo.html";
+$capfileName='capitolo'+$cap_num+'.html';
+$capfilePath=''+$back_path+"capitolo.html";
 $outputString='../../output.txt';
 Copy-Item $capfilePath $capfileName;
 $cmd='C:\Users\federick\Documents\Dev\GitHub\patente\manuale-patente\exebro.exe';
-Start-Process -Wait $cmd -ArgumentList "$min, $max, $outputString" -NoNewWindow;
+Start-Process -Wait $cmd -ArgumentList "$min_num, $max_num, $outputString" -NoNewWindow;
 # Changing page title
-(Get-Content $capfileName).Replace('<title>Capitolo 0</title>', '<title>Capitolo '+$capNum+'</title>') | Set-Content $capfileName;
+(Get-Content $capfileName).Replace('<title>Capitolo 0</title>', '<title>Capitolo '+$cap_num+'</title>') | Set-Content $capfileName;
 # Insert string created in capitolo file
 (Get-Content $capfileName).Replace('changehere', (Get-Content $outputString)) | Set-Content $capfileName;
 
 Write-Host "";
 
 # Generate "Argomento" files
-for ($counter=$min; $counter -lt $max+1; $counter++) {
+for ($counter=$min_num; $counter -lt $max_num+1; $counter++) {
     # Check for 0
     if ($counter -lt 10) {
         $prec='0'+($counter-1);
@@ -43,30 +59,35 @@ for ($counter=$min; $counter -lt $max+1; $counter++) {
             $prec='0'+($counter-1);
         }
     }
-    $filename=$name+'_'+$copyFile;
-    $fileprec=$prec+'_'+$copyFile;
-    $filesucc=$succ+'_'+$copyFile;
+    $filename=$name+'_'+$argomento_file;
+    $fileprec=$prec+'_'+$argomento_file;
+    $filesucc=$succ+'_'+$argomento_file;
     Copy-Item $totalpath $filename
     Write-Host "Created file" $filename;
     
     # Change file text
     # Change Chapter name
-    (Get-Content $filename).Replace('<title>Capitolo 0</title>', '<title>Capitolo '+$capNum+'</title>') | Set-Content $filename;
+    (Get-Content $filename).Replace('<title>Capitolo 0</title>', '<title>Capitolo '+$cap_num+'</title>') | Set-Content $filename;
     # Change Image name
     (Get-Content $filename).Replace('<img src="img/00.png" alt="">', '<img src="img/'+$name+'.png" alt="">') | Set-Content $filename;
     # Change Back-Image name
-    if ($counter -ne $min) {
+    if ($counter -ne $min_num) {
         (Get-Content $filename).Replace('<a href="back_argument">', '<a href="'+$fileprec+'">') | Set-Content $filename;
     } else {
-        (Get-Content $filename).Replace('<a href="back_argument">', '<a href="capitolo'+$capNum+'.html">') | Set-Content $filename;
+        (Get-Content $filename).Replace('<a href="back_argument">', '<a href="capitolo'+$cap_num+'.html">') | Set-Content $filename;
         (Get-Content $filename).Replace('<h2>argomento precedente</h2>', '<h2>torna indietro</h2>') | Set-Content $filename;
     }
     # Change Next-Image name
-    if ($counter -ne $max) {
+    if ($counter -ne $max_num) {
         (Get-Content $filename).Replace('<a href="next_argument">', '<a href="'+$filesucc+'">') | Set-Content $filename;
     } else {
-        (Get-Content $filename).Replace('<a href="next_argument">', '<a href="../capitolo'+($capNum+1)+'/capitolo'+($capNum+1)+'.html">') | Set-Content $filename;
+        (Get-Content $filename).Replace('<a href="next_argument">', '<a href="../capitolo'+($cap_num+1)+'/capitolo'+($cap_num+1)+'.html">') | Set-Content $filename;
         (Get-Content $filename).Replace('<h2>argomento successivo</h2>', '<h2>capitolo successivo</h2>') | Set-Content $filename;
     }
 }
 
+if (Test-Path -Path '../cache.txt') {
+    Remove-Item '../cache.txt';
+}
+Set-Location $home_path;
+Remove-Item 'output.txt';
