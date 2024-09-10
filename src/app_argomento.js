c69f368616694c3b926ac5dc58a89261591eb3d9
Federico Panarotto <104077886+federicopanarotto@users.noreply.github.com>
2024-09-11 01:03:17 +0200

    push fix assert with fetch

--- src/app_argomento.js
+++ src/app_argomento.js
@@ -1,4 +1,3 @@
-import data from "./content.json" assert { type: "json"};
 import { img_dir, img_ext, ifMine10 } from "./app_global.js";
 
 const title = document.querySelector("title");
@@ -8,59 +7,51 @@ const text_area = document.querySelector("#text-area");
 const back_button = document.querySelector("#back");
 const next_button = document.querySelector("#next");
 const back_button_text = document.querySelector("#back-text");
-const next_button_text = document.querySelector("#next-text")
+const next_button_text = document.querySelector("#next-text");
 
 let capitolo = parseInt(localStorage.getItem("capitolo"));
 let img_counter = parseInt(localStorage.getItem("argomento"));
 
-function updatePage() {
+function updatePage(data) {
     text_title.innerHTML = data.capitoli[capitolo].argomenti[img_counter].title;
-    img_area.src = `${img_dir}cap${capitolo+1}_${ifMine10(img_counter+1)}${img_ext}`
+    img_area.src = `${img_dir}cap${capitolo + 1}_${ifMine10(img_counter + 1)}${img_ext}`;
     text_area.innerHTML = data.capitoli[capitolo].argomenti[img_counter].content;
     text_area.classList.add(localStorage.getItem("argomento"));
     back_button_text.textContent = "argomento precedente";
     next_button_text.textContent = "argomento successivo";
     localStorage.setItem("argomento", img_counter);
+
+    if (img_counter == 0) {
+        back_button_text.textContent = "torna indietro";
+        next_button_text.textContent = "argomento successivo";
+    } else if (img_counter == data.capitoli[capitolo].argomenti.length - 1) {
+        next_button_text.textContent = "capitolo successivo";
+        back_button_text.textContent = "argomento precedente";
+    } else {
+        back_button_text.textContent = "argomento precedente";
+        next_button_text.textContent = "argomento successivo";
+    }
 }
 
-title.textContent = `Capitolo ${capitolo+1}`;
-title.textContent = `${img_counter}`; // detele this when you finish
-updatePage();
+fetch('./src/content.json')
+  .then(response => response.json())
+  .then(data => {
+    title.textContent = `Capitolo ${capitolo + 1}`;
+    title.textContent = `${img_counter}`; // Detele this when you finish
+    updatePage(data);
 
-if(img_counter == 0) {
-    back_button_text.textContent = "torna indietro";
-    next_button_text.textContent = "argomento successivo";
-} else if(img_counter == data.capitoli[capitolo].argomenti.length-1) {
-    next_button_text.textContent = "capitolo successivo";
-    back_button_text.textContent = "argomento precedente";
-} else {
-    back_button_text.textContent = "argomento precedente";
-    next_button_text.textContent = "argomento successivo";
-}
+    next_button.addEventListener("click", () => {
+        if (img_counter < data.capitoli[capitolo].argomenti.length - 1) {
+            img_counter++;
+            updatePage(data);
+        }
+    });
 
-next_button.addEventListener("click", () => {
-    if(img_counter < data.capitoli[capitolo].argomenti.length) {
-        img_counter++;
-        updatePage();
-    }
-});
-
-back_button.addEventListener("click", () => {
-    if(img_counter >= 0) {
-        img_counter--;
-        updatePage();
-    }
-});
-
-// if(img_counter == 0) { 
-//     setTimeout(() => {
-//         back_button.href = "capitolo.html";
-//     }, 0);
-// } 
-// if(img_counter == data.capitoli[capitolo].argomenti.length-1) {
-//     setTimeout(() => {
-//         next_button.href = "capitolo.html";
-//         capitolo++;
-//         localStorage.setItem("capitolo", capitolo);
-//     }, 0);
-// }
+    back_button.addEventListener("click", () => {
+        if (img_counter > 0) {
+            img_counter--;
+            updatePage(data);
+        }
+    });
+  })
+  .catch(error => console.error('Error loading JSON:', error));
--- src/app_capitolo.js
+++ src/app_capitolo.js
@@ -1,4 +1,3 @@
-import data from "./content.json" assert { type: "json"};
 import { img_dir, img_ext, ifMine10 } from "./app_global.js";
 
 const title = document.querySelector("title");
@@ -7,31 +6,36 @@ const list = document.querySelector("#list");
 let capitolo = parseInt(localStorage.getItem("capitolo"));
 let img_counter = parseInt(localStorage.getItem("argomento"));
 
-title.textContent = `Capitolo ${capitolo+1}`;
+title.textContent = `Capitolo ${capitolo + 1}`;
 
-for(let i = 0; i < data.capitoli[capitolo].argomenti.length; i++) {
-    const a = document.createElement("a");
-    const element = document.createElement("element");
-    const div = document.createElement("div");
-    const img = document.createElement("img");
-    const h1 = document.createElement("h1");
+fetch('./src/content.json')
+  .then(response => response.json())
+  .then(data => {
+    for (let i = 0; i < data.capitoli[capitolo].argomenti.length; i++) {
+      const a = document.createElement("a");
+      const element = document.createElement("element");
+      const div = document.createElement("div");
+      const img = document.createElement("img");
+      const h1 = document.createElement("h1");
 
-    list.appendChild(a);
-    a.appendChild(element);
-    a.href = "argomento.html";
-    a.id = i;
-    element.appendChild(div);
-    div.classList.add("container-img");
-    div.appendChild(img);
-    img.src = `${img_dir}cap${capitolo+1}_${ifMine10(i+1)}${img_ext}`;
-    element.appendChild(h1);
-    h1.textContent = data.capitoli[capitolo].argomenti[i].title
-}
+      list.appendChild(a);
+      a.appendChild(element);
+      a.href = "argomento.html";
+      a.id = i;
+      element.appendChild(div);
+      div.classList.add("container-img");
+      div.appendChild(img);
+      img.src = `${img_dir}cap${capitolo + 1}_${ifMine10(i + 1)}${img_ext}`;
+      element.appendChild(h1);
+      h1.textContent = data.capitoli[capitolo].argomenti[i].title;
+    }
 
-let elements = document.querySelectorAll("a");
-elements.forEach((element) => {
-    element.addEventListener("click", () => {
+    let elements = document.querySelectorAll("a");
+    elements.forEach((element) => {
+      element.addEventListener("click", () => {
         img_counter = parseInt(element.id);
         localStorage.setItem("argomento", img_counter);
+      });
     });
-});
+  })
+  .catch(error => console.error('Error loading JSON:', error));
